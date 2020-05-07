805d3435fa552f342120487d279dd905b1b29f4e
Luis Fco López Gómez <luisfcolg.13@gmail.com>
2020-05-06 22:35:30 -0500

    - Created app file

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+
+app = Flask(__name__)             # create an app instance
+
+
+@app.route("/")                   # at the end point /
+def hello():                      # call method hello
+    return "Hello World!"         # which returns "hello world"
+
+
+if __name__ == "__main__":        # on running python app.py
+    # Schema()
+    app.run(debug=True)           # run the flask app
